15a9dd7af8ecff328eefadb4f148e0aaf45b73f3
Vihaan-Taparia <mayataparia@gmail.com>
2025-04-03 21:25:00 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,39 @@
+#datatype
+a = 5
+print("type of a:",type(a))
+w = 37.9
+print("type of w:",type(w))
+c ="coding"
+print("type of c:",type(c))
+d =True
+print("type of d:",type(d))
+
+
+#type casting means converting one data type to another
+
+#float too int
+w=int(w)
+print(w)
+
+#int to float
+a=float(a)
+print(a)
+
+z=97
+
+print(a+z)
+
+
+
+#reverse string
+
+
+text = str(input("Enter a string:  "))
+revtext = text[::-1]
+
+print("Reverse of given string is:")
+print(revtext)
+
+
+ 
+
